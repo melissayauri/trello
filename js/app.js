6bc7eaae8a0fc670549a74702eaf5719230c85ad
melissayauri <yaurimelissa@gmail.com>
2017-11-15 00:37:31 -0500

    incorporando el boton añadir lista

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/*
+window.addEventListener('load',function(){
+  var button = document.getElementById('entrada');
+  button.addEventListener('click',enterForm);
+
+  }
+})*/
+
+window.addEventListener('load',function(){
+  button.addEventListener('click',enterForm);
+
+})
+var button = document.getElementById('entrada');
+function enterForm(){
+button.style.display="none";
+}
