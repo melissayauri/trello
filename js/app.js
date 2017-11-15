42479964cad79d6819224c1a570bdfd37488e6a0
melissayauri <yaurimelissa@gmail.com>
2017-11-15 02:01:34 -0500

    incorporando el formulario

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
-/*
+
 window.addEventListener('load',function(){
   var button = document.getElementById('entrada');
-  button.addEventListener('click',enterForm);
-
+  var formul = document.getElementById('formul')
+  button.addEventListener('click', enterForm);
+  function enterForm(){
+    /*llamando a ocultar el primer boton*/
+  button.classList.add('enter');
+  /*aparece el formulario*/
+  formul.style.display = "inline-block";
   }
-})*/
-
+  /*
+  var formul = document.getElementById('formul');
+  formul.addEventListener('click', aparecer);
+  function aparecer(){
+    button.classList.toggle('enter');
+  }*/
+})
+/*
 window.addEventListener('load',function(){
   button.addEventListener('click',enterForm);
 
@@ -13,4 +24,4 @@ window.addEventListener('load',function(){
 var button = document.getElementById('entrada');
 function enterForm(){
 button.style.display="none";
-}
+}*/
